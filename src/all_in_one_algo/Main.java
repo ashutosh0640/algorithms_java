b1550a7967b5dbd6ed9142ef6318ea1f8821f80b
ashutosh0640 <ashulook1013@gmail.com>
2024-07-29 10:28:46 +0530

    merge sort

---
+++ src/all_in_one_algo/Main.java
@@ -0,0 +1,9 @@
+package all_in_one_algo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/all_in_one_algo/MergeSort.java
@@ -0,0 +1,10 @@
+package all_in_one_algo;
+
+public class MergeSort {
+	
+	public static <T> T mergeSort(T input) {
+		return input;
+		
+	}
+
+}
